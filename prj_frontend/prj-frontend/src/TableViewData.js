7c75e7bcfd71f80f509f6f8310652fd68e30354f
dmaindola <maindola.dhruv@gmail.com>
2023-02-19 22:26:50 +0530

    stashed changes

--- prj_frontend/prj-frontend/src/TableViewData.js
+++ prj_frontend/prj-frontend/src/TableViewData.js
@@ -1,50 +1,46 @@
-import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import axios from 'axios';
-import {useState , useEffect} from 'react';
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-const TableViewData = ({tabledata}) => {
+const TableViewData = ({ tabledata }) => {
   return (
     <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>security name</TableCell>
-                <TableCell align="right">has position</TableCell>
-                {/* <TableCell align="right">Fat</TableCell>
-                <TableCell align="right">Carbs</TableCell>
-                <TableCell align="right">Protein</TableCell> */}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-            {/* fragment */}
-            <> 
-              {
-              tabledata.map((value,index) => {
-                return ( 
-                    <TableRow
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>security name</TableCell>
+            <TableCell align="right">has position</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <>
+            {tabledata.map((value, index) => {
+              return (
+                <TableRow
                   key={index}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
                   <TableCell component="th" scope="row">
                     {value.securityName}
                   </TableCell>
-                  <TableCell align="right">{String(value.hasPosition).toUpperCase()}</TableCell>
+                  <TableCell align="right">
+                    {String(value.hasPosition).toUpperCase()}
+                  </TableCell>
                 </TableRow>
-                )
-              })}
-                </>
-                </TableBody>
-            
-          </Table>
-        </TableContainer>
-  )
-}
+              );
+            })}
+          </>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
-export default TableViewData
+export default TableViewData;
