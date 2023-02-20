17f047acb6536cc0976f943823dec8315f99b7a6
dmaindola <maindola.dhruv@gmail.com>
2023-02-20 14:46:54 +0530

    display tables working for equity and bond as well

--- prj_frontend/prj-frontend/src/Bond Tabs/BondsTabs.js
+++ prj_frontend/prj-frontend/src/Bond Tabs/BondsTabs.js
@@ -1,36 +1,149 @@
+// import React from "react";
+// import Tabs from "@mui/material/Tabs";
+// import Tab from "@mui/material/Tab";
+
+// const EquityTabs = () => {
+//   let [currentValue, setValue] = React.useState(0);
+
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//   };
+
+//   return (
+//     <div>
+//       <Tabs
+//         variant="fullWidth"
+//         value={currentValue}
+//         onChange={handleChange}
+//         TabIndicatorProps={{ style: { backgroundColor: "#02111B" } }}
+//         sx = {{bgcolor : "#D3D0CB"}}
+//       >
+//         <Tab label={<span style={{ color: "#02111B" }}>Secruity Summary</span>}></Tab>
+//         <Tab label={<span style={{ color: "#02111B" }}>Secruity Identifier</span>}></Tab>
+//         <Tab label={<span style={{ color: "#02111B" }}>Secruity Details</span>}></Tab>
+//         <Tab label={<span style={{ color: "#02111B" }}>Risk</span>}></Tab>
+//         <Tab label={<span style={{ color: "#02111B" }}>Regulatory Details</span>}></Tab>
+//         <Tab label={<span style={{ color: "#02111B" }}>Reference Data</span>}></Tab>
+//         <Tab label={<span style={{ color: "#02111B" }}>Put Schedule</span>}></Tab>
+//         <Tab label={<span style={{ color: "#02111B" }}>Pricing and Analytics</span>}></Tab>
+//         <Tab label={<span style={{ color: "#02111B" }}>Call Schedule</span>}></Tab>
+
+//       </Tabs>
+//     </div>
+//   );
+// };
+
+// export default EquityTabs;
+
+
 import React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import { TabPanel } from "@mui/lab";
+import GetBonds from "./../GetBonds";
+import { AppBar } from "@mui/material";
+import TableViewData from "../TableViewData";
+import axios from "axios";
 
-const EquityTabs = () => {
+const BondTabs = () => {
   let [currentValue, setValue] = React.useState(0);
+  
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  return (
-    <div>
-      <Tabs
-        variant="fullWidth"
-        value={currentValue}
-        onChange={handleChange}
-        TabIndicatorProps={{ style: { backgroundColor: "#02111B" } }}
-        sx = {{bgcolor : "#D3D0CB"}}
-      >
-        <Tab label={<span style={{ color: "#02111B" }}>Secruity Summary</span>}></Tab>
-        <Tab label={<span style={{ color: "#02111B" }}>Secruity Identifier</span>}></Tab>
-        <Tab label={<span style={{ color: "#02111B" }}>Secruity Details</span>}></Tab>
-        <Tab label={<span style={{ color: "#02111B" }}>Risk</span>}></Tab>
-        <Tab label={<span style={{ color: "#02111B" }}>Regulatory Details</span>}></Tab>
-        <Tab label={<span style={{ color: "#02111B" }}>Reference Data</span>}></Tab>
-        <Tab label={<span style={{ color: "#02111B" }}>Pricing Details</span>}></Tab>
-        <Tab label={<span style={{ color: "#02111B" }}>Divident History</span>}></Tab>
-        <Tab label={<span style={{ color: "#02111B" }}>Divident History</span>}></Tab>
+ 
 
-      </Tabs>
-    </div>
+  const [bondData , setData] = React.useState([""]);
+  const result = async (url) => {
+       await axios.get(url)
+        .then((response) => {
+            setData(response.data);
+            console.log("this is message  " , response , result["data"]);
+        });
+    }
+
+    
+React.useEffect(() => {
+  switch (currentValue) {
+    case 0:
+      result("http://localhost:5144/getbond/b-sec-summary");
+      break;
+    case 1:
+      result("http://localhost:5144/getbond/b-sec-identifier");
+      break;
+    case 2: 
+      result("http://localhost:5144/getbond/b-sec-details");
+      break;
+    case 3: 
+      result("http://localhost:5144/getbond/b-risk");
+      break;  
+    case 4: 
+      result("http://localhost:5144/getbond/b-reg-details");
+      break;
+    case 5: 
+      result("http://localhost:5144/getbond/b-ref-data");
+      break;
+    case 6: 
+      result("http://localhost:5144/getbond/b-put-schedule");
+      break;
+    case 7: 
+      result("http://localhost:5144/getbond/b-prc-anl");
+      break; 
+    case 8: 
+      result("http://localhost:5144/getbond/b-call-schedules");
+      break;    
+
+    default:
+      break;
+  }
+},[currentValue]); 
+
+  return (
+    <>
+      <AppBar position="sticky">
+        <Tabs
+          variant="fullWidth"
+          value={currentValue}
+          onChange={handleChange}
+          TabIndicatorProps={{ style: { backgroundColor: "#02111B" } }}
+          sx={{ bgcolor: "#D3D0CB" }}
+        >
+          <Tab
+            label={<span style={{ color: "#02111B" }}>Secruity Summary</span>}
+          >
+            {" "}
+          </Tab>
+          <Tab
+            label={
+              <span style={{ color: "#02111B" }}>Secruity Identifier</span>
+            }
+          ></Tab>
+          <Tab
+            label={<span style={{ color: "#02111B" }}>Secruity Details</span>}
+          ></Tab>
+          <Tab label={<span style={{ color: "#02111B" }}>Risk</span>}></Tab>
+          <Tab
+            label={<span style={{ color: "#02111B" }}>Regulatory Details</span>}
+          ></Tab>
+          <Tab
+            label={<span style={{ color: "#02111B" }}>Reference Data</span>}
+          ></Tab>
+          <Tab
+            label={<span style={{ color: "#02111B" }}>Put Schedule</span>}
+          ></Tab>
+          <Tab
+            label={<span style={{ color: "#02111B" }}>Pricing and Analytics</span>}
+          ></Tab>
+          <Tab
+            label={<span style={{ color: "#02111B" }}>Call Schedule</span>}
+          ></Tab>
+        </Tabs>
+      </AppBar>
+      <TableViewData tabledata={bondData}/>
+    </>
   );
 };
 
-export default EquityTabs;
+export default BondTabs;
